8491c248b7dbc6b1be9d0dc524ccadffa91cabc9
xavi <xavisp6@gmail.com>
2025-04-23 21:09:25 +0200

    Afegir el context al buscar la playlist a la base de dades

--- app/src/main/java/com/ivax/descarregarvideos/ui/playlists/PlaylistsViewModel.kt
+++ app/src/main/java/com/ivax/descarregarvideos/ui/playlists/PlaylistsViewModel.kt
@@ -3,12 +3,16 @@ package com.ivax.descarregarvideos.ui.playlists
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import com.ivax.descarregarvideos.entities.relationships.PlaylistWithSavedVideos
 import com.ivax.descarregarvideos.repository.MediaPlayerRepository
 import com.ivax.descarregarvideos.repository.VideoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -39,13 +43,24 @@ class PlaylistsViewModel @Inject constructor(private val videoRepository: VideoR
        return playlists.value!!.filter { it.playlist.name!!.lowercase().contains( playlistName.lowercase()) }
     }
     fun shuffle(playlistId : Int) {
-        var playlistShuffle= videoRepository.getByPlaylistIdWithPositions(playlistId)
-        mediaPlayerRepository.addPlaylistShuffle(playlistShuffle)
+        viewModelScope.launch(Dispatchers.IO) {
+            var playlistShuffle = videoRepository.getByPlaylistIdWithPositions(playlistId)
+            withContext(Dispatchers.Main) {
+                mediaPlayerRepository.addPlaylistShuffle(playlistShuffle)
+            }
+
+        }
 
     }
     fun playAll(playlistId : Int) {
-        var playlistsWithPos=videoRepository.getByPlaylistIdWithPositions(playlistId).sortedBy { it.position }
-        mediaPlayerRepository.addPlaylist(playlistsWithPos)
+        viewModelScope.launch(Dispatchers.IO) {
+            var playlistsWithPos=videoRepository.getByPlaylistIdWithPositions(playlistId).sortedBy { it.position }
+            withContext(Dispatchers.Main) {
+                mediaPlayerRepository.addPlaylist(playlistsWithPos)
+            }
+
+        }
+
     }
     fun addPlaylistShuffle(videos: PlaylistWithSavedVideos) {
 
