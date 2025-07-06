07a907a5c3bd8ddcfe4d31292eea5aeb2d7df6e6
xaviserentill <xavisp6@gmail.com>
2025-07-06 14:11:29 +0200

    Canviar l'alpha dels colors del tema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.1"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ksp {
--- app/src/main/java/com/ivax/descarregarvideos/ui/theme/Color.kt
+++ app/src/main/java/com/ivax/descarregarvideos/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.ivax.descarregarvideos.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkBackground = Color(35,35,35)
-val GreySecondari=Color(46,46,46)
-val TextWhite=Color(255,255,255)
+val DarkBackground = Color(35,35,35,255)
+val GreySecondari=Color(46,46,46,255)
+val TextWhite=Color(255,255,255,255)
 
